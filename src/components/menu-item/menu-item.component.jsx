bbcebdfb54e4ea54746c95867a423ff9ba8ecdaf
Ragnar Öst <ragnar@patronum.se>
2021-11-15 13:44:25 +0100

    Updated react-router-dom to v6, updated syntax and added functionallaty to links

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-// destruction of the props for easy acces
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-);
+export function MenuItem({ title, imageUrl, size, linkUrl }) {
+  let navigate = useNavigate();
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl}`,
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
 
-export default MenuItem;
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+}
